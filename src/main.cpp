9bdc911a7e1a2bd85001bf8eebeb2b5ea64c5d62
Kent Delaney <kgdelaney@att.net>
2013-10-31 13:37:17 -0600

    We can check that certain numbers are powers of 2.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+bool isPowOf2( unsigned int i );
+
 int main( int argc, char** argv )
 {
     /* Check for proper usage */
@@ -13,9 +15,16 @@ int main( int argc, char** argv )
         return -1;
     }
 
-    /* Open config file and glean configuration */
-
     /* SIMULATE... THAT... CACHE!!!! */
 
     return 0;
+}
+
+bool isPowOf2( unsigned int i )
+{
+    /* This snippet was found at:
+     * http://stackoverflow.com/questions/600293/
+     *    how-to-check-if-a-number-is-a-power-of-2
+     */
+    return (i != 0) && ((i & (i - 1)) == 0);
 }
