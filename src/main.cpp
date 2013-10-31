7ac83339dc64c481fce10c35187c28e06450c691
Kent Delaney <kgdelaney@att.net>
2013-10-31 13:29:43 -0600

    Checked for proper usage.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include <fstream>
 
+using namespace std;
+
 int main( int argc, char** argv )
 {
     /* Check for proper usage */
+    if( argc != 3 )
+    { 
+        cout<< "Usage:\n\t";
+        cout<< "cachesim configFile traceFile\n";
+        return -1;
+    }
 
     /* Open config file and glean configuration */
 
