b6dc38b9fb1df5aaf7ec1f96a2a2c5977f695040
Kent Delaney <kgdelaney@att.net>
2013-10-31 13:16:57 -0600

    Added a makefile

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,13 @@
-int main()
+#include <iostream>
+#include <fstream>
+
+int main( int argc, char** argv )
 {
+    /* Check for proper usage */
+
+    /* Open config file and glean configuration */
+
+    /* SIMULATE... THAT... CACHE!!!! */
+
     return 0;
 }
