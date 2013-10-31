0c8cb1e00cedeecb1db53ecd84becefe9fbc0932
Thomas George Barker <tbarker@rainbow.nmt.edu>
2013-10-31 13:28:04 -0600

    hellol

---
+++ src/simulator.h
@@ -0,0 +1,16 @@
+class simulator 
+{
+    protected:
+        int lineSize;
+        int associativity;
+        int dataSize;
+        int replacePolicy;
+        int missPenalty;
+        int writeAllocate;
+    public:
+    /* CTOR */
+    /* getters and setter insert here */
+    /* function to simulate contents of file */
+    
+
+};
